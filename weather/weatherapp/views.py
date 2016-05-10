8c8f81bedac647b77b39ea060651d96ed45726ab
0x1bad1d3a <0x1bad1d3a@gmail.com>
2016-05-10 00:48:18 -0700

    add simple template

--- weather/weatherapp/views.py
+++ weather/weatherapp/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import History
 
-# Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the weather index.")
+    history_list = History.objects.order_by('id')[:10]
+    context = {'history_list' : history_list}
+    return render(request, 'index.html', context)
+
+def search(request, location):
+    response = "You're searching for the weather at {}".format(location)
+    return HttpResponse(response)
