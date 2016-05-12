44671c1ae8340a5064587300bf3487bf0453068c
0x1bad1d3a <0x1bad1d3a@gmail.com>
2016-05-11 21:59:13 -0700

    add API operations

--- weather/weatherapp/urls.py
+++ weather/weatherapp/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
         url(r'^$', views.index, name='index'),
+        url(r'^accounts/login/$', auth_views.login, name="login"),
         url(r'^(?P<location>.*)/$', views.search, name="search"),
         ]
--- weather/weatherapp/views.py
+++ weather/weatherapp/views.py
@@ -1,12 +1,54 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import History
 
+import requests
+
+@login_required(login_url='/weatherapp/accounts/login/')
 def index(request):
-    history_list = History.objects.order_by('id')[:10]
-    context = {'history_list' : history_list}
+
+    location = request.POST['location'] if 'location' in request.POST else None
+    mapJson = None
+    weatherJson = None
+
+    if location:
+
+        mapUri = 'https://maps.googleapis.com/maps/api/geocode/json?address={}&key=AIzaSyCLjAV18w2FPLa2z9vU2DgqK9cHe9h6J_s'.format(location)
+        mapJson = requests.get(mapUri).json()
+
+        if mapJson['status'] == 'OK':
+
+            lat = mapJson['results'][0]['geometry']['location']['lat']
+            lng = mapJson['results'][0]['geometry']['location']['lng']
+
+            weatherUri = 'https://api.forecast.io/forecast/beec74efaeee64d55513d049112d385b/{},{}'.format(lat, lng)
+            weatherJson = requests.get(weatherUri).json()
+
+        h = History(user=request.user, search_history=location, search_result=mapJson)
+        h.save()
+
+    history_list = History.objects.order_by('-id')[:10]
+
+    context = {
+                'history_list' : history_list,
+                'location' : location,
+                'mapJson' : mapJson,
+                'weatherJson' : weatherJson
+              }
     return render(request, 'index.html', context)
 
+def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None and user.is_active:
+        login(request, user)
+        return redirect('index')
+    else:
+        return HttpResponse("Error logging in")
+
 def search(request, location):
-    response = "You're searching for the weather at {}".format(location)
-    return HttpResponse(response)
+    context = { 'location' : location }
+    return render(request, 'search.html', context)
